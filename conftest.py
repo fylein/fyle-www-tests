0f75f063d51fac9c9f3f3a843714e180642f2448
Krishna <apple@apples-Air.Dlink>
2021-05-04 15:42:21 +0530

    added custom arguments in pytest to swtich between base urls/environment

--- conftest.py
+++ conftest.py
@@ -1,14 +1,34 @@
 import time
 import logging
 import pytest
+import os
 
 from common.utils import create_browser, resize_browser, get_browser_name
 
 logger = logging.getLogger(__name__)
 
+BASE_URL = {
+    "prod" : "https://www.fylehq.com",
+    "stag1" : "https://staging1.fyle.tech",
+    "stag2" : "https://staging2.fyle.tech",
+    "local" : "http://localhost:4000"
+}
+
+#Parser adoption to use custom arguments from command line
+def pytest_addoption(parser):
+    parser.addoption("--url", action="store", default="prod")
+
+@pytest.fixture(scope='session')
+def url(request):
+    base_url = request.config.option.url
+    if base_url is None:
+        pytest.skip()
+    return base_url
+
+
 @pytest.fixture(scope='module')
-def base_url():
-    return 'https://ww2.fylehq.com'
+def base_url(url):
+    return BASE_URL[url]
 
 #Assigning capabilities based on where the test case is going to run.(Remote or Local)
 BROWSER_CAPABALITIES = [None] if get_browser_name() != 'remote' else [
