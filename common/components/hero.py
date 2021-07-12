1d31a286c7349f9b023f10379b21830dce3f1832
Krishna <apple@apples-Air.Dlink>
2021-07-12 14:10:59 +0530

    Added compnay page test cases

--- common/components/hero.py
+++ common/components/hero.py
@@ -8,9 +8,13 @@ def assert_h1_spacing(browser, section_class, value=20):
     heading = browser.find(f'//section[contains(@class, "gradient-background") and contains(@class, "{section_class}")]//h1')
     assert_spacing('bottom', heading, value)
 
-def assert_subtext_spacing(browser, section_class, value=40):
-    span = browser.find(f'//section[contains(@class, "gradient-background") and contains(@class, "{section_class}")]//div[contains(@class, "single-column")]//p')
-    assert_spacing('bottom', span, value)
+def assert_subtext_spacing(browser, section_class, value=40, mid_span=10):
+    spans = browser.find_many(f'//section[contains(@class, "gradient-background") and contains(@class, "{section_class}")]//div[contains(@class, "single-column")]//p')
+    for i, el in enumerate(spans):
+        if i == (len(spans)-1):
+            assert_spacing('bottom', el, value)
+        else:
+            assert_spacing('bottom', el, mid_span)
 
 def assert_img(browser, img_width, section_class):
     img = browser.find(f'//section[contains(@class, "gradient-background") and contains(@class, "{section_class}")]//img[contains(@class, "gradient-hero-img")]')
@@ -27,11 +31,12 @@ def assert_button_spacing(browser, section_class, value=40, cta=True):
         assert_demo_cta(browser, button_xpath)
 
 def assert_customer_logo(browser, logo_width, section_xpath=None, logo_xpath=None, spacing_top=None, spacing_bottom=None):
-    logo_section = browser.find(section_xpath, scroll=True)
-    if spacing_top:
-        assert_spacing('top', logo_section, spacing_top)
-    if spacing_bottom:
-        assert_spacing('bottom', logo_section, spacing_bottom)
+    if section_xpath:
+        logo_section = browser.find(section_xpath, scroll=True)
+        if spacing_top:
+            assert_spacing('top', logo_section, spacing_top)
+        if spacing_bottom:
+            assert_spacing('bottom', logo_section, spacing_bottom)
     
     def check_logo_display():
         logo = browser.find(logo_xpath, scroll=True)
---
+++ pages/company/test_company_laptop.py
@@ -0,0 +1,25 @@
+from common.asserts import *
+from .common.company import *
+from common.components.hero import *
+from . import laptop_browser as browser
+
+def test_hero_section(browser):
+    section_class = 'investor-logos'
+    assert_h1_spacing(browser, section_class)
+    assert_subtext_spacing(browser, section_class, value=60)
+    assert_customer_logo(browser, 931, logo_xpath='//section[contains(@class, "fy-p-0")]//div[contains(@class, "container")]//img[contains(@class, "d-md-block")]')
+
+def test_why_fyle_section(browser):
+    assert_why_fyle_section(browser, 100, 40, 30, 262, 847)
+
+def test_stat_section(browser):
+    assert_stat_section(browser, 50, 255, 60)
+
+def test_fyle_journey(browser):
+    assert_fyle_journey(browser, 100, '//section[contains(@class, "fyle-journey")]//div[contains(@class, "fy-xl-row")]//div[contains(@class, col)]//img[contains(@class, "d-md-block")]', 880, 40, 60)
+
+def test_bottom_banner_cta(browser):
+    assert_bottom_banner_cta(browser)
+
+def test_page_overflow(browser):
+    assert_overflowing(browser)
